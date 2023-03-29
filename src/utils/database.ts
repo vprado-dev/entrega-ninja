2c5e7adf6caa2317cd0dc57081fdc945df437c14
Vinicius Prado <vinicavagna@gmail.com>
2023-03-28 22:25:18 -0300

    feat: add database object

---
+++ src/utils/database.ts
@@ -0,0 +1,34 @@
+// @ts-nocheck
+import knex from "knex";
+import path from "path";
+
+const MIGRATIONS_PATH = path.join(
+  __dirname,
+  "..",
+  "..",
+  "database",
+  "migrations",
+);
+const MIN_POOL = 2;
+const MAX_POOL = 20;
+
+export const database = knex({
+  client: "pg",
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    port: +process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database:
+      process.env.NODE_ENV === "test"
+        ? `test-${process.env.POSTGRES_DB}`
+        : process.env.POSTGRES_DB,
+  },
+  pool: {
+    min: MIN_POOL,
+    max: MAX_POOL,
+  },
+  migrations: {
+    directory: MIGRATIONS_PATH,
+  },
+});
